7abd74e01c78524b66fdb43d92abce3eed5850c2
garciab1 <162745376+garciab1@users.noreply.github.com>
2024-09-01 19:47:54 -0600

    Agregar Nuevos Productos
    Datos Estáticos

---
+++ app/Http/Controllers/AgregarProductoAdmin_Controller.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AgregarProductoAdmin_Controller extends Controller
+{
+    public function AgregarProductoAdmin(){
+        return view('AgregarProductoAdmin');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,20 @@
 <?php
 
+use App\Http\Controllers\AgregarVentaAdmin_Controller;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeControler;
 use App\Http\Controllers\Inicioadmin_Controller;
 use App\Http\Controllers\InicioUser_Controller;
 use App\Http\Controllers\RealizarVenta_Controller;
 use App\Http\Controllers\RealizarVentaAdmin_Controller;
+use App\Http\Controllers\AgregarProducto_Controller;
+use App\Http\Controllers\AgregarProductoAdmin_Controller;
 
 Route::get('/', [HomeControler::class,'index']);
 Route::get('/inicio_admin',[Inicioadmin_Controller::class,'inicio_admin'])->name('IniAdmin');
 Route::get('/inicio_user',[InicioUser_Controller::class,'inicio_user'])->name('IniUser');
 Route::get('/realizarVenta',[RealizarVenta_Controller::class,'realizarVenta'])->name('RealizarVenta');
 Route::get('/realizarVentaAdmin',[RealizarVentaAdmin_Controller::class,'realizarVentaAdmin'])->name('RealizarVenta');
+Route::get('/AgregarProducto',[AgregarProducto_Controller::class,'AgregarProducto'])->name('AgregarProducto');
+Route::get('/AgregarProductoAdmin',[AgregarProductoAdmin_Controller::class,'AgregarProductoAdmin'])->name('AgregarProductoAdmin');
 
